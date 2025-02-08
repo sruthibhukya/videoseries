50e7a96895d38f05477705052bf7bdfafec58222
sruthibhukya <121614628+sruthibhukya@users.noreply.github.com>
2025-02-08 20:23:13 +0530

    fixing all register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 import { User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
-import {user} from "../models/user.model.js"
+// import {user} from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async(req,res)=>
         //check for user creation
         //return res
           const {fullName,email,username,password}= req.body
-          console.log("email:",email);
+          //console.log("email:",email);
           if (
             [fullName, email, username, password].some((field) => field?.trim() === "")
         ) {
@@ -30,7 +30,7 @@ const registerUser = asyncHandler(async(req,res)=>
         if (existedUser) {
             throw new ApiError(409, "User with email or username already exists")
         }
-        //console.log(req.files);
+        // console.log(req.files);
         const avatarLocalPath = req.files?.avatar[0]?.path;
         //const coverImageLocalPath = req.files?.coverImage[0]?.path;
     
